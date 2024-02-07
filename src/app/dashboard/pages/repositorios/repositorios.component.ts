930823adb439fc48049f2b93ec0adb82c0b359fa
cinta <cintatafur@gmail.com>
2024-02-07 12:09:41 +0100

    🧱 Create(structute proyect)

---
+++ src/app/dashboard/pages/repositorios/repositorios.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './repositorios.component.html',
+  styles: ``
+})
+export class RepositoriosComponent {
+
+}
---
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './sidemenu.component.html',
+  styleUrl: './sidemenu.component.scss'
+})
+export class SidemenuComponent {
+
+}
